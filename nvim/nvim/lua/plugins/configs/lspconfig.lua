7166964355df8f73b1be167796adb42358670061
mmihira <mmihira.w@gmail.com>
2025-03-17 21:07:51 +1100

    add cpp config

--- nvim/nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/nvim/lua/plugins/configs/lspconfig.lua
@@ -45,6 +45,45 @@ lspconfig.html.setup({})
 -- HTML
 lspconfig.ts_ls.setup(defaultconfig)
 
+local cppconfig = {
+  on_attach = lsplib.mk_config().on_attach,
+  keys = {
+    { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+  },
+  root_dir = function(fname)
+    return require("lspconfig.util").root_pattern(
+      "Makefile",
+      "configure.ac",
+      "configure.in",
+      "config.h.in",
+      "meson.build",
+      "meson_options.txt",
+      "build.ninja"
+    )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname) or require(
+      "lspconfig.util"
+    ).find_git_ancestor(fname)
+  end,
+  capabilities = {
+    offsetEncoding = { "utf-16" },
+  },
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=llvm",
+  },
+  init_options = {
+    usePlaceholders = true,
+    completeUnimported = true,
+    clangdFileStatus = true,
+  },
+}
+-- C++
+lspconfig.clangd.setup(cppconfig)
+
 -- Hide the dianostic virtual text
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = false,
